d1005f72415ed2578289a94d5f7b79bd36c2cfcd
Diogo Alves <diogo-alves1998@hotmail.com>
2023-08-02 14:47:18 +0100

    react

---
+++ Section 25-26-27-28-29-30-31 - React/random-boxes/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Box = props => {
+  const boxStyle = {
+    width: '100px',
+    height: '100px',
+    backgroundColor: props.color
+  }
+  return (<div style={boxStyle} />);
+}
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    const boxColors = [];
+    for(let i = 0; i < 32; i++){
+      boxColors.push(props.allColors[Math.floor(Math.random()*props.allColors.length)])
+    }
+    this.state = {boxColors}
+    setInterval(() => {
+      const newBoxColors = [...this.state.boxColors]
+      newBoxColors[Math.floor(Math.random()*newBoxColors.length)] = props.allColors[Math.floor(Math.random()*props.allColors.length)]
+      this.setState({boxColors: newBoxColors})
+    },300)
+  }
+  
+  render() {
+    const containerStyle = {
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'row',
+    };
+
+    return (
+      <div className="App" style={containerStyle}>
+        {this.state.boxColors.map(color => <Box color={color}/>)}
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+};
+
+export default App;
